26f6323212287fad9b2b541cd69b7ec1a62159d9
Николай <onoprienko.kolya477@mail.ru>
2025-08-09 09:59:30 +0300

    Версия 1.1 1. Добавление моделирования движения. 2. Добавлена возможность выбрать начальную скорость заряда. Для этого нужно зажать кнопку мыши при добавлении заряда, тянуть её в нужную сторону и отпустить. При этом для наглядности появляется вектор. От размера этого вектора зависит модуль скорости, от направления, собственно, зависит направление начальной скорости. 3. Добавлена возможность изменять массу заряда. 4. Добавлена возможность вкл/выкл моделирование электрического поля и/или движения. Для этого были созданы кнопки, меняющие цвет в зависимости от того, активна она или нет и от того, наведена ли на неё мышка или нет. 5. От величины заряда теперь зависит цвет: чем больше модуль заряда, тем цвет ярче. 6. Диаметр заряда зависит теперь от массы. 7. В верхнем левом углу теперь отображается время.
    8. В коде теперь графические функции вынесены в файл func_for_pg.py.

--- Power_lines_program/func_for_pg.py
+++ Power_lines_program/func_for_pg.py
@@ -1,15 +1,18 @@
 import numpy as np
 from numba import jit, prange
+import pygame as pg
 
+# физические функции:
 
-@jit(fastmath=True, nopython=True)
+# одна силовая линия
+@jit(fastmath=True, nopython=True, cache=True)
 def tension(number_of_iterations, x_list, y_list, q_list, sign_list,
             distance_sqr, ax, ay, k, x, y, l):
 
     i = 0
 
     while np.amin(distance_sqr) >= 1 and \
-            i <= number_of_iterations:
+            i < number_of_iterations:
 
         try:
             E_x = sign_list[k] * np.sum(
@@ -41,7 +44,7 @@ def tension(number_of_iterations, x_list, y_list, q_list, sign_list,
     return ax, ay
 
 
-
+# электрическое поле всех зарядов
 def power_lines(number_of_lines, number_of_iterations, n, x_list, y_list, q_list, sign_list):
 
     segments = np.full((number_of_lines * n + n, number_of_iterations + 2, 2), np.nan, dtype=np.float64)
@@ -73,3 +76,118 @@ def power_lines(number_of_lines, number_of_iterations, n, x_list, y_list, q_list
             iteration += 1
 
     return segments
+
+
+# Движение всех зарядов
+def movement(n, x_list, y_list, q_list, m_list,vx_list, vy_list, t):
+
+    # Считаем, что q - в мкКл, масса в г. Таким образом коэффициент в законе Кулона будет численно равен 9
+
+    acceleration_x_list = np.array(())
+    acceleration_y_list = np.array(())
+    dt = 1/60
+
+    for k in range(n):
+
+        q_list_not_k = np.delete(q_list, k)
+        x_list_not_k = np.delete(x_list, k)
+        y_list_not_k = np.delete(y_list, k)
+
+        acceleration_x = ((-9 * q_list[k]) / m_list[k]) * np.sum((q_list_not_k * (x_list_not_k - x_list[k])) / (
+                    ((x_list_not_k - x_list[k]) ** 2 + (y_list_not_k - y_list[k]) ** 2) ** (3 / 2)))
+
+        acceleration_y = ((-9 * q_list[k]) / m_list[k]) * np.sum((q_list_not_k * (y_list_not_k - y_list[k])) / (
+                    ((x_list_not_k - x_list[k]) ** 2 + (y_list_not_k - y_list[k]) ** 2) ** (3 / 2)))
+
+        acceleration_x_list = np.append(acceleration_x_list, acceleration_x)
+        acceleration_y_list = np.append(acceleration_y_list, acceleration_y)
+
+    x_list = x_list + (vx_list * dt + acceleration_x_list * dt**2 / 2) * 1000
+    y_list = y_list + (vy_list * dt + acceleration_y_list * dt**2 / 2) * 1000
+
+    vx_list = vx_list + acceleration_x_list * dt
+    vy_list = vy_list + acceleration_y_list * dt
+
+    t = t + dt
+
+    return x_list, y_list, vx_list, vy_list, t
+
+
+#Графические функции:
+
+
+#Кнопка
+def button(display, pos, width, height, var, LKM, buttonText=' '):
+    fillColors = {
+        'normal': (240, 48, 61),
+        'hover': var * np.array([100, 163, 102]) + (not var) * np.array([163, 100, 100]),
+        'pressed': (63, 204, 68)
+    }
+    mousePos = pg.mouse.get_pos()
+    buttonSurface = pg.Surface((width, height))
+    buttonRect = pg.Rect(pos[0], pos[1], width, height)
+    font = pg.font.Font(size=22)
+    buttonSurf = font.render(buttonText, True, (255, 255, 255))
+
+    if var:
+        buttonSurface.fill(fillColors['pressed'])
+    else:
+        buttonSurface.fill(fillColors['normal'])
+
+    if buttonRect.collidepoint(mousePos):
+        buttonSurface.fill(fillColors['hover'])
+        if LKM:
+            if var:
+                var = False
+                buttonSurface.fill(fillColors['normal'])
+            else:
+                var = True
+                buttonSurface.fill(fillColors['pressed'])
+
+    buttonSurface.blit(buttonSurf, [
+        buttonRect.width / 2 - buttonSurf.get_rect().width / 2,
+        buttonRect.height / 2 - buttonSurf.get_rect().height / 2
+    ])
+    display.blit(buttonSurface, buttonRect)
+    pg.draw.rect(display, (255, 255, 255), (pos[0], pos[1], width, height), 2)
+    return var
+
+# cлайдeр / ползунок
+def slider(display, position, slider_len, units, active_unit, mouse, LKM, variable, dist_mouse):
+    pg.draw.line(display, (255, 255, 255), position, (position[0] + slider_len, position[1]), 3)
+    step = slider_len // units
+    dist = np.linalg.norm(np.array(mouse) - np.array(position) - np.array((step * active_unit, 0)))
+    if dist > dist_mouse * step:
+        pg.draw.circle(display, (0, 155, 255), (position[0] + step * active_unit, position[1]), 5)
+    else:
+        pg.draw.circle(display, (0, 255, 255), (position[0] + step * active_unit, position[1]), 10)
+        if LKM:
+            if 1 <= (mouse[0] - position[0]) // step <= units:
+                variable = (mouse[0] - position[0]) // step
+    return variable
+
+# вектор
+def vec(display, start, end, color, vec_size=10):
+
+    pg.draw.aaline(display, color, start, end)
+
+    if end[0] != start[0]:
+        angle = np.arctan((end[1]-start[1])/(end[0]-start[0]))
+
+        pg.draw.aaline(display, color, end,
+        [end[0] - np.sign(end[0] - start[0]) * vec_size * np.cos(np.pi / 6 - angle),
+                end[1] + np.sign(end[0] - start[0]) *  vec_size * np.sin(np.pi / 6 - angle)], 3)
+        pg.draw.aaline(display, color, end,
+        [end[0] - np.sign(end[0] - start[0]) * vec_size * np.cos(np.pi / 6 + angle),
+                end[1] - np.sign(end[0] - start[0]) * vec_size * np.sin(np.pi / 6 + angle)], 3)
+    else:
+        pg.draw.aaline(display, color, end,
+                          [end[0] - vec_size * np.sin(np.pi / 6),
+                           end[1] - np.sign(end[1] - start[1]) * vec_size * np.cos(np.pi / 6)], 3)
+        pg.draw.aaline(display, color, end,
+                           [end[0] + vec_size * np.sin(np.pi / 6),
+                            end[1] - np.sign(end[1] - start[1]) * vec_size * np.cos(np.pi / 6)], 3)
+
+
+
+
--- Power_lines_program/pygame_interface.py
+++ Power_lines_program/pygame_interface.py
@@ -2,33 +2,28 @@ import pygame as pg
 from pygame import RESIZABLE, Surface, FULLSCREEN
 from pygame.display import set_caption, is_fullscreen
 import numpy as np
-from func_for_pg import power_lines
+from func_for_pg import power_lines, movement, button, vec, slider
 
-
+# предварительное создание некоторых "физических" констант
 x_list = np.array(())
 y_list = np.array(())
 q_list = np.array(())
+m_list = np.array(())
+vx_list = np.array(())
+vy_list = np.array(())
 sign_list = np.array(())
 s_list = []
 c_list = []
+power_lines_simulation = True
+movement_simulation = True
+LKM_ONE = False
 
-q = 1
+q = 10
+m = 100
+t = 0
 
+# Инициализация пай гейм
 pg.init()
-
-def slider(display, position, slider_len, units, active_unit, mouse, LKM, variable):
-    pg.draw.line(display, WHITE, position, (position[0] + slider_len, position[1]), 3)
-    step = slider_len // units
-    dist = np.linalg.norm(np.array(mouse) - np.array(position) - np.array((step * active_unit, 0)))
-    if dist > 2 * step:
-        pg.draw.circle(display, (0, 155, 255), (position[0] + step * active_unit, position[1]), 5)
-    else:
-        pg.draw.circle(display, TURQUOISE, (position[0] + step * active_unit, position[1]), 10)
-        if LKM:
-            if 1 <= (mouse[0] - position[0]) // step <= units:
-                variable = (mouse[0] - position[0]) // step
-    return variable
-
 
 # Параметры графики и графические константы
 FPS = 60
@@ -46,15 +41,18 @@ BLUE = (48, 128, 245)
 WHITE = (255, 255, 255)
 TURQUOISE = (0, 255, 255)
 
-f1 = pg.font.Font('cmunrm.ttf', 20)
-f2 = pg.font.Font('cmunrm.ttf', 24)
-f3 = pg.font.Font('cmunrm.ttf', 22)
+f1 = pg.font.Font(size=22)
+f2 = pg.font.Font(size=30)
+f3 = pg.font.Font(size=28)
+
+# Инструкция в интерфейсе
 string_0 = f1.render('Управление: ', True, WHITE)
 string_1 = f1.render('Добавить положительный заряд: ЛКМ', True, WHITE)
 string_2 = f1.render('Добавить отрицательный заряд: ПКМ', True, WHITE)
 string_3 = f1.render('Очистить экран от зарядов: ESC', True, WHITE)
 string_4 = f1.render('Развернуть на полный экран: F', True, WHITE)
 string_5 = f2.render('Модуль заряда', True, WHITE)
+string_5_5 = f2.render('Масса', True, WHITE)
 string_6 = f3.render('Количество линий на заряд', True, WHITE)
 string_7 = f3.render('Количество итераций', True, WHITE)
 
@@ -75,9 +73,15 @@ clock = pg.time.Clock()
 play = True
 mouse = pg.mouse.get_pos()
 LKM = False
+PKM = False
+
+# инициализация функции моделирования электрического поля для быстрого её запуска пользователем
+segments = power_lines(number_of_lines, number_of_iterations, 1, np.array([457.]), np.array([366.]), np.array([10.]), np.array([1.]))
 
 pg.display.update()
 
+
+# Основной цикл (Прорисовка каждого кадра)
 while play:
 
     display.fill(GRAY)
@@ -89,29 +93,61 @@ while play:
         if i.type == pg.QUIT:
             play = False
 
-        # События мыши
+        # События мыши:
+        # Нажатия клавиши мыши
         if i.type == pg.MOUSEBUTTONDOWN:
             if i.button == 1:
                 LKM = True
+                LKM_ONE = True
             if field_rect.collidepoint(mouse):
                 if i.button == 1:
+                    # Добавление положительного заряда
                     x_list = np.append(x_list, mouse[0] - scal_x * 25)
                     y_list = np.append(y_list, mouse[1] - scal_y * 25)
                     q_list = np.append(q_list, q)
+                    m_list = np.append(m_list, m)
                     sign_list = np.append(sign_list, 1)
-                    s_list.append(q * 4)
-                    c_list.append(RED)
+                    s_list.append(m/100 + 6)
+                    c_list.append((q + 155, 48, 68))
+
+                    # Начало вектора начальной скорости
+                    mouse_start = np.array(mouse)
+
                 if i.button == 3:
+                    # Добавление отрицательного заряда
                     x_list = np.append(x_list, mouse[0] - scal_x * 25)
                     y_list = np.append(y_list, mouse[1] - scal_y * 25)
                     q_list = np.append(q_list, -1 * q)
+                    m_list = np.append(m_list, m)
                     sign_list = np.append(sign_list, -1)
-                    s_list.append(q * 4)
-                    c_list.append(BLUE)
+                    s_list.append(m/100 + 6)
+                    c_list.append((28, 48, q + 155))
 
+                    PKM = True
+                    # Начало вектора начальной скорости
+                    mouse_start = np.array(mouse)
+
+        # Отпускание клавиши мыши
+        # Создание координаты конца вектора скорости и добавление модуля скорости заряда
         if i.type == pg.MOUSEBUTTONUP:
             if i.button == 1:
                 LKM = False
+                end_arrow = np.array(mouse)
+                if field_rect.collidepoint(mouse):
+                    try:
+                        vx_list = np.append(vx_list, ((mouse - np.array(mouse_start))[0])/1000)
+                        vy_list = np.append(vy_list, ((mouse - np.array(mouse_start))[1])/1000)
+                    except:
+                        pass
+            if i.button == 3:
+                PKM = False
+                if field_rect.collidepoint(mouse):
+                    try:
+                        vx_list = np.append(vx_list, ((mouse - np.array(mouse_start))[0])/1000)
+                        vy_list = np.append(vy_list, ((mouse - np.array(mouse_start))[1])/1000)
+                    except:
+                        pass
+
 
         # События изменения размеров окна
         if i.type == pg.VIDEORESIZE:
@@ -119,6 +155,7 @@ while play:
 
         # События клавиатуры
         if i.type == pg.KEYDOWN:
+            # Полноэкранный режим
             if i.key == pg.K_f:
                 is_fullscreen = not is_fullscreen
                 if is_fullscreen:
@@ -128,10 +165,14 @@ while play:
                 else:
                     current_size = last_size
                     display = pg.display.set_mode(current_size, RESIZABLE)
+            # Удаление всех зарядов
             if i.key == pg.K_ESCAPE:
                 x_list = np.array(())
                 y_list = np.array(())
                 q_list = np.array(())
+                m_list = np.array(())
+                vx_list = np.array(())
+                vy_list = np.array(())
                 sign_list = np.array(())
                 s_list = []
                 c_list = []
@@ -146,15 +187,18 @@ while play:
     virtual_display.blit(string_3, (905, 115))
     virtual_display.blit(string_4, (905, 140))
     virtual_display.blit(string_5, (970, 195))
-    virtual_display.blit(string_6, (918, 263))
-    virtual_display.blit(string_7, (950, 330))
+    virtual_display.blit(string_5_5, (1020, 263))
+    virtual_display.blit(string_6, (918, 330))
+    virtual_display.blit(string_7, (950, 397))
 
-    string_8 = f3.render(str(q), True, TURQUOISE)
-    string_9 = f3.render(str(number_of_lines), True, TURQUOISE)
-    string_10 = f3.render(str(number_of_iterations), True, TURQUOISE)
+    string_8 = f1.render(str(q) + ' мкКл', True, TURQUOISE)
+    string_8_8 = f1.render(str(m) + ' г', True, TURQUOISE)
+    string_9 = f1.render(str(number_of_lines), True, TURQUOISE)
+    string_10 = f1.render(str(number_of_iterations), True, TURQUOISE)
     virtual_display.blit(string_8, (1200, 220))
-    virtual_display.blit(string_9, (1200, 285))
-    virtual_display.blit(string_10, (1200, 351))
+    virtual_display.blit(string_8_8, (1200, 285))
+    virtual_display.blit(string_9, (1200, 351))
+    virtual_display.blit(string_10, (1200, 417))
 
     # Отображение на дисплей других элементов
     scal_x = current_size[0] / width
@@ -170,29 +214,55 @@ while play:
 
 
     # Слайдеры
-    q = slider(display, [scal_x * 925, scal_y * 235], scal_x * 255, 20, q, mouse, LKM, q)
+    q = slider(display, [scal_x * 925, scal_y * 235], scal_x * 255, 50, q/2, mouse, LKM, q/2, 5) * 2
 
-    number_of_lines = int(slider(display, [scal_x * 925, scal_y * 300], scal_x * 255, 20, number_of_lines, mouse,
-                             LKM, number_of_lines))
-    number_of_iterations = int(slider(display, [scal_x * 925, scal_y * 365], scal_x * 255, 20, number_of_iterations//200, mouse,
-                             LKM, number_of_iterations/200) * 200)
+    m = round(slider(display, [scal_x * 925, scal_y * 300], scal_x * 255, 250, m/4, mouse, LKM, m/4, 20) * 4, 1)
 
-    # Отображение зарядов и линий
+    number_of_lines = int(slider(display, [scal_x * 925, scal_y * 365], scal_x * 255, 20, number_of_lines, mouse,
+                             LKM, number_of_lines, 2))
+    number_of_iterations = int(slider(display, [scal_x * 925, scal_y * 430], scal_x * 255, 20, number_of_iterations//200, mouse,
+                             LKM, number_of_iterations/200, 2) * 200)
 
+    # Кнопки
+    button_1 = button(display, (scal_x * 925, scal_y * 480), scal_x * 255, scal_y * 25, power_lines_simulation, LKM_ONE, 'Симуляция электрического поля')
+    power_lines_simulation = button_1
+
+    button_2 = button(display, (scal_x * 925, scal_y * 510), scal_x * 255, scal_y * 25, movement_simulation, LKM_ONE,
+                     'Симуляция движения')
+    movement_simulation = button_2
+
+    # Отображение векторов скорости, линий электрического поля и зарядов
     n = len(q_list)
     x_list = np.array(x_list)
     y_list = np.array(y_list)
     q_list = np.array(q_list)
+    m_list = np.array(m_list)
     sign_list = np.array(sign_list)
-    segments = power_lines(number_of_lines, number_of_iterations, n, x_list, y_list, q_list, sign_list)
 
-    if segments.shape[0] >= 2:
-        for segment in segments:
-            pg.draw.aalines(field, TURQUOISE, False, segment)
+    # Отображение моделирования электрического поля
+    if power_lines_simulation:
+        segments = power_lines(number_of_lines, number_of_iterations, n, x_list, y_list, q_list, sign_list)
+        if segments.shape[0] >= 2:
+            for segment in segments:
+                pg.draw.aalines(field, TURQUOISE, False, segment)
 
+    # Отображения зарядов
     for i in range(len(q_list)):
         pg.draw.circle(field, c_list[i], [x_list[i], y_list[i]], s_list[i])
         pg.draw.circle(field, WHITE, [x_list[i], y_list[i]], s_list[i], 2)
 
+    # Отображение вектора
+    if (LKM or PKM) and field_rect.collidepoint(mouse):
+        try:
+            vec(field, np.array(mouse_start) - np.array([25 * scal_x, 25 * scal_y]), mouse - np.array([25 * scal_x, 25 * scal_y]), WHITE, vec_size=10)
+        except:
+            pass
+    # Моделирование движения зарядов
+    elif movement_simulation:
+        x_list, y_list, vx_list, vy_list, t = movement(n, x_list, y_list, q_list, m_list, vx_list, vy_list, t)
+        time = f1.render(str(round(t, 1)) + ' c', True, DARK_GRAY, WHITE)
+        field.blit(time, (5, 5))
+
+    LKM_ONE = False
     clock.tick(FPS)
     pg.display.update()
